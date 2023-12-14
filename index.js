6554ddfda07f41c2e0c7711642f90791134a072d
sein <seinkane01@gmail.com>
2023-12-14 12:18:28 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
 
-// Write your code here
+var num1=31;
+var num2=2;
+var multiply=num1*num2;
+console.log(multiply);
+
+var random=Math.floor(Math.random()*1)+1
+console.log(random)
+
+let num3=48;
+let  num4=11;
+let quotient=Math.floor(48/11)
+let mod=48%11
+
+
+const number=[18,19,20]
+const max=Math.max(18,19,20)
+
+
